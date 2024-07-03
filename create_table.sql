71905caabb3287e9423db53c5c0530c739503835
epodak <epodak@gmail.com>
2024-07-04 04:17:35 +0800

    :+1: Improve 更新数据库描述语句自动递增列AUTO_INCREMENT必须是主键PRIMARY_KEY或唯一键UNIQUE_KEY

--- create_table.sql
+++ create_table.sql
@@ -1,30 +1,32 @@
-/* mysql  Ver 14.14 Distrib 5.7.24, for linux-glibc2.12 (x86_64) using  EditLine wrapper */
+-- 创建数据库
+CREATE DATABASE industry DEFAULT CHARACTER SET utf8;
 
-create database industry DEFAULT CHARACTER SET utf8;
-use industry;
+-- 使用该数据库
+USE industry;
 
+-- 创建一维表 dim_industry
 CREATE TABLE dim_industry (
-  id int AUTO_INCREMENT,
-  industry_id varchar(12),
-  name varchar(1000),
-  parent_id varchar(12),
-  level_type int,
-  description varchar(4000)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  industry_id VARCHAR(12),
+  name VARCHAR(1000),
+  parent_id VARCHAR(12),
+  level_type INT,
+  description VARCHAR(4000)
 );
 
+-- 创建二维表 dim_industry2
 CREATE TABLE dim_industry2 (
-  id                        int auto_increment,
-  category_industry_id      varchar(10),
-  category_name             varchar(1000),
-  category_desc             varchar(1000),
-  large_class_industry_id   varchar(10),
-  large_class_name          varchar(1000),
-  large_class_desc          varchar(1000),
-  mid_class_industry_id     varchar(10),
-  mid_class_name            varchar(1000),
-  mid_class_desc            varchar(1000),
-  small_class_industry_id   varchar(10),
-  small_class_name          varchar(1000),
-  small_class_desc          varchar(1000),
-  primary key(id)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  category_industry_id VARCHAR(10),
+  category_name VARCHAR(1000),
+  category_desc VARCHAR(1000),
+  large_class_industry_id VARCHAR(10),
+  large_class_name VARCHAR(1000),
+  large_class_desc VARCHAR(1000),
+  mid_class_industry_id VARCHAR(10),
+  mid_class_name VARCHAR(1000),
+  mid_class_desc VARCHAR(1000),
+  small_class_industry_id VARCHAR(10),
+  small_class_name VARCHAR(1000),
+  small_class_desc VARCHAR(1000)
 );
